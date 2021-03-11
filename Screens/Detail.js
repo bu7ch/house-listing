4ea0fc277b76f61f469f45280d97c5334c61ff3d
Rodolphe <dutchr@live.fr>
2021-03-11 17:15:04 +0100

    finish but need polish into icons & device choice

--- Screens/Detail.js
+++ Screens/Detail.js
@@ -1,24 +1,10 @@
-import { View, Text, Button, StyleSheet } from "react-native";
-import { StatusBar } from "expo-status-bar";
+
 import React from "react";
+import ListingDetail from "../components/Listing/ListingDetail.android";
 
-
-const Detail = ({navigation}) => {
-  const item = navigation.getParam('item', {});
-  return (
-  <View style={styles.container}>
-    <Text>{item.title}</Text>
-    
-  </View>
-  )
+const Detail = ({ navigation }) => {
+  const item = navigation.getParam("item", {});
+  return <ListingDetail item={item} />;
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 export default Detail;
---
+++ components/Listing/ListingDetail.ios.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Image, Text, View, Dimensions } from 'react-native';
+import styled from 'styled-components/native';
+
+const ListingDetailWrapper = styled(View)`
+display:flex;
+`;
+
+const Details = styled(View)`
+position:absolute;
+top:0;
+padding:5%;
+width:100%;
+background: rgba(0,0,255,0.1);
+`;
+
+export const Title = styled(Text)`
+flex-wrap: wrap;
+  width: 99%;
+  font-size: 30px;
+`;
+
+export const Price = styled(Text)`
+font-weight:bold;
+font-size:20px
+color: blue
+`;
+export const Thumbnail = styled(Image)`
+width:100%;
+height: ${Dimensions.get('window').height}
+`;
+
+
+const ListingDetail = ({item}) => (
+  <ListingDetailWrapper>
+    <Thumbnail source={{ uri: item.thumbnail}}/>
+    <Details>
+      <Title>{ item.title }</Title>
+      <Price>{ item.price}</Price>
+    </Details>
+  </ListingDetailWrapper>
+)
+export default ListingDetail
